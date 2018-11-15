ea6f2e21afe7108ef904729c362897175cfb4c55
Vincent <vincentquan64@gmail.com>
2018-11-14 19:56:24 -0800

    API readme

--- server/index.js
+++ server/index.js
@@ -20,6 +20,59 @@ app.get('/api/volumes/symbols/:id', function(req, res){
     res.end(JSON.stringify(data));
   })
 })
+
+app.delete('/api/volumes/symbols/:id', function(req, res) {
+  PriceVolume.findOneAndRemove({id: req.params.id}, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(202).send("Record successfully deleted")
+    }
+  })
+})
+
+app.patch('/api/volumes/symbols/:id', function(req, res) {
+  PriceVolume.findOneAndUpdate({id: req.params.id}, {$set: req.body}, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.send('Successful update');
+    }
+  })
+})
+
+app.post('/api/volumes/symbols/', function(req, res) {
+  PriceVolume.create(req.body, function(req, res) {
+    if (err) {
+      res.status(400).send(err);
+    } else {
+      res.send('Post succesful!')
+    }
+  })
+})
+
+// app.post('/api/volumes/symbols/:id', function(req, res) {
+//   PriceVolume.create({
+//     id: req.params.id,
+//     symbol: req.body.symbol,
+//     name: req.body.name,
+//     prices: req.body.prices,
+//     volumes: req.body.volumes,
+//     lowest: req.body.lowest,
+//     highest: req.body.highest,
+//     averagePrice: req.body.averagePrice,
+//     currentPrice: req.body.currentPrice
+//   }, (err, data) => {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       console.log('Post successful!');
+//     }
+//   })
+// })
+
+
+
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });
